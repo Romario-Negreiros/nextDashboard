229beb3e43cf98d650229d406349d41f573ecae8
Romario Negreiros <nromario482@gmail.com>
2022-11-17 21:19:21 -0300

    Use usePersistedState hook to save selected theme Import ThemeProvider component, wrap Layout and Outlet components in it and pass theme state props Create toggleTheme function and pass it to Layout component as props Import DefaultTheme interface from styled-components and import custom dark and light themes

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -2,13 +2,27 @@ import React from 'react'
 
 import { Outlet } from 'react-router-dom'
 import Layout from '../../layout'
+import { ThemeProvider } from 'styled-components'
+
+import { usePersistedState } from '../../modules/hooks'
+
+import { dark, light } from '../../styles/themes'
+
+import type { DefaultTheme } from 'styled-components'
 
 const Root: React.FC = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark)
+
+  const toggleTheme = () => {
+    setTheme(theme => (theme.title === 'light' ? dark : light))
+  }
   return (
     <>
-      <Layout>
-        <Outlet />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout toggleTheme={toggleTheme}>
+          <Outlet />
+        </Layout>
+      </ThemeProvider>
     </>
   )
 }
