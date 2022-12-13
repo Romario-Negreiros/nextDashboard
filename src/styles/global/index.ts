c6a5653791bc29bf48652a19749f7a806f2a69f3
Romario Negreiros <nromario482@gmail.com>
2022-12-12 21:34:32 -0300

    Add default transition to all elements

--- src/styles/global/index.ts
+++ src/styles/global/index.ts
@@ -5,9 +5,13 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: Verdana, Geneva, Tahoma, sans-serif
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    transition: all 0.6s
   }
   html {
     font-size: 10px;
+  }
+  .body-hide {
+
   }
 `
