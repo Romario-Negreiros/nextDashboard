738faf5617c178174dfa5138468ad2d507f36306
Romario Negreiros <nromario482@gmail.com>
2022-12-15 12:06:17 -0300

    Remove theme state

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -2,23 +2,15 @@ import { Outlet } from 'react-router-dom'
 import Layout from '../../layout'
 import { ThemeProvider } from 'styled-components'
 
-import { usePersistedState } from '../../modules/hooks'
-
-import { dark, light } from '../../styles/themes'
+import theme from '../../styles/theme'
 
 import type { FC } from 'react'
-import type { DefaultTheme } from 'styled-components'
 
 const Root: FC = () => {
-  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark)
-
-  const toggleTheme = () => {
-    setTheme(theme => (theme.title === 'light' ? dark : light))
-  }
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Layout toggleTheme={toggleTheme}>
+        <Layout>
           <Outlet />
         </Layout>
       </ThemeProvider>
