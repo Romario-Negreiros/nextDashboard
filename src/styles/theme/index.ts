84bc2793e51201b90c3e9d4a116cbf875dd9bf7b
Romario Negreiros <nromario482@gmail.com>
2022-12-15 12:05:51 -0300

    Delete light and dark themes files, create a single theme file and remove title property from DefaultTheme interface in styled.d.ts

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,27 @@
+import type { DefaultTheme } from 'styled-components'
+
+const theme: DefaultTheme = {
+  bgs: {
+    primary: '#fff',
+    secondary: '#f5f5f5'
+  },
+  contrasts: {
+    primary: '#000',
+    secondary: '#333'
+  },
+  elements: {
+    red: '#d7263d',
+    orange: '#f46036',
+    blueish: '#2e294e',
+    green: '#1b998b',
+    yellowish: '#c5d86d'
+  },
+  gradients: {
+    top: 'linear-gradient(0deg, #d7263dff, #f46036ff, #2e294eff, #1b998bff, #c5d86dff)',
+    right: 'linear-gradient(90deg, #d7263dff, #f46036ff, #2e294eff, #1b998bff, #c5d86dff',
+    bottom: 'linear-gradient(180deg, #d7263dff, #f46036ff, #2e294eff, #1b998bff, #c5d86dff)',
+    left: 'linear-gradient(270deg, #d7263dff, #f46036ff, #2e294eff, #1b998bff, #c5d86dff)'
+  }
+}
+
+export default theme
---
+++ src/styles/theme/styled.d.ts
@@ -0,0 +1,27 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    bgs: {
+      primary: string
+      secondary: string
+    }
+    contrasts: {
+      primary: string
+      secondary: string
+    }
+    elements: {
+      red: string
+      orange: string
+      blueish: string
+      green: string
+      yellowish: string
+    }
+    gradients: {
+      top: string
+      right: string
+      bottom: string
+      left: string
+    }
+  }
+}
